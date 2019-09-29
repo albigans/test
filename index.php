7f2a1f165370129293e7fcdd5615e993a3e704da
albigans <175471@smktelkommedan.sch.id>
2019-09-30 00:26:58 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php session_start(); ?>
+<html>
+	<head>
+		<title>FUCK-BOY</title>
+		<link rel="icon" type="image/png" href="img/hekertod3.png">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+		<script src="bootstrap/js/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="sweetalert/dist/sweetalert.css">
+		<script type="text/javascript" src="sweetalert/dist/sweetalert.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<style type="text/css">
+			body{
+				background-color: #000;
+
+			}
+
+			.jumbotron {
+				background-image: url("img/themehekertod.jpg");
+				color: #000;
+				padding: 100px 25px;
+				font-family: Montserrat, sans-serif;
+				height: 1000px;
+			}
+			.navbar{
+				margin-bottom: 0;
+				background-color:black;
+
+			}
+			.footer{
+				height: 0px;
+			}
+			.modal-header, h4, .close {
+				background-color: #5cb85c;
+				color:white !important;
+				text-align: center;
+				font-size: 30px;
+			}
+  			.modal-footer {
+      			background-color: #f9f9f9;
+  			}
+		</style>
+	</head>
+	<body>
+		<script>
+			window.swal("WELCOME","Ini cuma fake anonymous yaa tod!","success");
+			
+		</script>
+		<nav class="navbar navbar-inverse">
+			<div class="navbar navbar-light bg-light">
+		  
+		  <div class="container-fluid"> 	
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="index.php">Home</a>
+		    </div>
+		    <ul class="nav navbar-nav">
+		      <li><a href="https://en.wikipedia.org/wiki/Software_cracking">Crack</a></li>
+		      <li><a href="https://en.wikipedia.org/wiki/Website_defacement">Deface</a></li>
+		      <li><a href="https://en.wikipedia.org/wiki/Carding_(fraud)">Carding</a></li>
+		      <li><a href="info.php">More Info</a></li>
+		    </ul>
+		    <ul class="nav navbar-nav navbar-right">
+		    	
+		      	<li><a href="#"><span class="glyphicon glyphicon-user"></span> Signup</a></li>
+		      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+
+
+		     
+		    </ul>
+		  </div>
+		</nav>
+		<div class="jumbotron text-center">
+		
+			<a href="http://www.hackthissite.org" class="btn btn-primary" style="font-family: consolas;"><span class="glyphicon glyphicon-edit"></span> Public Page</a>
+			
+		  		
+		  <h2><font color="white" style="font-family:  consolas">	Welcome to The Jungle ...</h2>
+		  <p>start your skills here!</p>
+		
+		  		<p><font color="gold">Register To be hacker now!</font>
+		   <a class="btn btn-info" href="#" role="button">Sign Up</a></p>
+		   
+		</div>
+		<div class="footer">
+			<center><p> Created By Biza Alzabizal @2019</p>
+			<a href="https://www.facebook.com/www.7777777777777777777777777777777777777777.co.id">Facebook</a>
+			<a href="https://www.instagram.com/_albizabizal/">Instagram</a>
+			
+		</center>
+		</div>
+
+	</body>
+</html>
---
+++ info.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<link rel="icon" type="img/png" href="img/hekertod3.png">
+	<link rel="stylesheet" type="text/css" href="style2.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+</head>
+<body>
+	
+	<section id="box-profile">
+		<div class="img-profile">
+			<div class="photo" style="background-image: url(img/hekertod3.png);">
+				</div>
+		</div>
+		<div class="description">
+			<h1>Anonymous People</h1>
+			<p>Front End Designer</p>
+			<a href="#" class="btn btn-lg btn-block btn-success">Contact</a>
+			<a href="index.php" class="btn btn-lg btn-block btn-info">Resume</a>
+		</div>
+		
+	</section><br/>
+	<section style="font-family: consolas; color: white;">
+		<center>
+			<h1>We Are Anonymous ..</h1>
+			<h1>Our Activities only Helping People on the Internet!</h1>
+			<h1>We didn't commit Crimes as social media say ..</h1>
+		</center>
+	</section>
+	
+
+</body>
+</html>
